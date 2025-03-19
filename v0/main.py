d7a70c924190498865bcb33f9ef3e6ceda984cb3
Afonso de Sousa <af.nunosousa@gmail.com>
2025-03-19 15:59:45 +0000

    v0

---
+++ v0/main.py
@@ -0,0 +1,22 @@
+# Streamlit frontend
+
+import streamlit as sl
+from model import view_parking
+
+
+
+def main():
+    view_parking("../data/Aerial View - Woburn Mall.mp4")
+    
+    #sl.header("Fraqtory")
+    #sl.subheader("Parking Spots")
+
+
+    #if sl.button("Next"):
+    #    sl.video(view_parking("data/Aerial View - Woburn Mall.mp4"))
+        
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ v0/model.py
@@ -0,0 +1,45 @@
+
+import cv2
+from utils.filter import *
+
+def check_parking_spots():  
+    mask = cv2.imread("../masks/crop.jpg", 0)
+
+    p_spots_bounding_boxes = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+    p_spots_positions = extract_spots_with_mask(p_spots_bounding_boxes)
+    
+    return len(p_spots_positions), p_spots_positions
+
+def view_parking(footage_path):
+
+    cap = cv2.VideoCapture(footage_path)
+    total_spots, spots_positions = check_parking_spots()
+
+    ret = True
+
+    while ret:
+        # to loop video 
+        #if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        #    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+        ret, frame = cap.read()
+        
+        for spot in spots_positions:
+            empty_counter = 0
+            x1, y1, w, h = spot
+            cropping = frame[y1:y1+h, x1:x1+w]
+            if spot_availability(cropping):
+                cv2.rectangle(frame, (x1,y1), (x1 + w, y1 + h), (0,255,0),2)
+                empty_counter += 1
+            else:
+                cv2.rectangle(frame, (x1,y1), (x1 + w, y1 + h), (0,0,255),2)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+        
+
+        
+    cap.release()
+    cv2.destroyAllWindows()
