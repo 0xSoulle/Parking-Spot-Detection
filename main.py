3f650065c345a464c91b5011f619ae9def9e9106
souLLe <af.nunosousa@gmail.com>
2025-07-23 00:23:04 +0100

    way more simple python script

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from cv2.gapi import video
 from ultralytics import YOLO
-from os import listdir
 
-
+# load custom model with trained weights
 model = YOLO("best.pt")
-# all data files
-files = [f for f in listdir("data") if f.endswith(".mp4")]
+ 
+# run inference for all files in the data dir
+results = model(source="data/*",show=True)
 
-video_index = 0
-while(True):
-    results = model(source="data/"+files[video_index],show=True)
-
-    # loop through data dir
-    video_index = video_index + 1
-    if (video_index >= len(files)):
-        video_index = 0
