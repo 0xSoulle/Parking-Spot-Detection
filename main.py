7d23285d86a1da3ac1a51a316d5cc90f96e38826
souLLe <af.nunosousa@gmail.com>
2025-09-01 15:52:34 +0100

    process prediction results with openCV

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from ultralytics import YOLO
+import cv2
 
 # load custom model with trained weights
 model = YOLO("best.pt")
  
+cap = cv2.VideoCapture(source="data/*")
 # run inference for all files in the data dir
-results = model(source="data/*",show=True)
+results = model(source="data/*",show=True, stream=True)
 
+
+for result in results:
+    result_log = open("result_log.txt", "rw")
+    boxes = result.boxes  
+    masks = result.masks  
+    keypoints = result.keypoints  
+    probs = result.probs 
+    obb = result.obb
+
+    result.show() 
